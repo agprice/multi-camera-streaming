e3a93be4853892d522d9c0e8b14853658cf30e64
Juan Davalos <j_c_j@ucsb.edu>
2020-02-17 10:38:47 -0800

    Simple program has been added to main to attempt to capture something

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace server
 {
@@ -6,7 +7,27 @@ namespace server
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try{
+                var process = new Process
+                {
+                    StartInfo = 
+                    {
+                        FileName = "ffmpeg",
+                        Arguments = "-f v4l2 -i /dev/video0 -framerate 20 -frames:v 20 -s 1366x768 -vcodec h264 -f mpegts pipe:1",
+                        UseShellExecute = false,
+                        RedirectStandardInput = true,
+                        RedirectStandardOutput = true
+                    }
+                };
+                process.Start();
+                var value = process.StandardOutput.ReadToEnd();
+                process.WaitForExit();
+                Console.WriteLine(value.Length);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
