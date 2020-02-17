b6ace632430db027081dae015b9d953f110f0bdd
anonymous <anonymous>
2020-02-17 15:16:00 -0800

    Got some basic screen capture to work

--- server/Server.cs
+++ server/Server.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
 
 namespace server
 {
-    class Server 
+    class Server
     {
         static void Main(string[] args)
         {
@@ -11,26 +12,40 @@ namespace server
         }
         static void test()
         {
-            try{
+            try
+            {
                 var process = new Process
                 {
-                    StartInfo = 
+                    StartInfo =
                     {
                         FileName = "ffmpeg",
-                        Arguments = "-hide_banner -loglevel error -f v4l2 -i /dev/video0 -framerate 20 -frames:v 20 -s 1366x768 -vcodec h264 -f mpegts pipe:1",
+                        Arguments = "-hide_banner -loglevel error -s 1920x1080 -framerate 30 -f x11grab -i :0.0+0,0 -frames:v 512 -vcodec h264 -preset ultrafast -f mpegts pipe:1",
                         UseShellExecute = false,
                         RedirectStandardOutput = true,
-                        RedirectStandardInput = true,
-                        RedirectStandardError = true,
+                        RedirectStandardInput = false,
+                        RedirectStandardError = false,
+                        CreateNoWindow = true
                     }
                 };
                 process.ErrorDataReceived += new DataReceivedEventHandler((o, e) => throw new ApplicationException(e.Data));
                 process.Start();
-                var value = process.StandardOutput.ReadToEnd();
-                process.WaitForExit();
-                Console.WriteLine(value.Length);
+                Console.WriteLine("Started process.");
+                BinaryWriter bw;
+                BinaryReader binaryReader;
+                try
+                {
+                    binaryReader = new BinaryReader(process.StandardOutput.BaseStream);
+                    bw = new BinaryWriter(new FileStream("test.mp4", FileMode.Create));
+                    process.StandardOutput.BaseStream.CopyTo(bw.BaseStream);
+                    bw.Flush();
+                    bw?.Close();
+                }
+                catch (IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
