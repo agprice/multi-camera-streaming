739d535bdb472b2da4b599c2187f7269baaf484a
Juan Davalos <j_c_j@ucsb.edu>
2020-02-19 14:04:20 -0800

    Code to packet readers

--- server/Classes/PacketReader/OptsPacketReader/FfmpegOptsPacketReader.cs
+++ server/Classes/PacketReader/OptsPacketReader/FfmpegOptsPacketReader.cs
@@ -1,9 +1,31 @@
+using System.Net.Sockets;
+using System.Text;
+
+using server.Interfaces.Options;
 using server.Interfaces.PacketReader.OptsPacketReader;
+
+using server.Classes.Options;
 
 namespace server.Classes.PacketReader.OptsPacketReader
 {
     public class FfmpegOptsPacketReader : IOptsPacketReader
     {
+        public FfmpegOptsPacketReader() { }
 
+        public IOptions receiveOptions(NetworkStream stream)
+        {
+            var buffer = new byte[256];
+            var message = new StringBuilder();
+
+            do {
+                stream.Read(buffer, 0, buffer.Length);
+                message.AppendFormat("{0}", Encoding.ASCII.GetString(buffer);
+            } while(stream.DataAvailable);
+
+            var ffmpegops = new FfmpegOptions();
+            ffmpegops.add(message.ToString());
+
+            return ffmpegops;
+        }
     }
 }
