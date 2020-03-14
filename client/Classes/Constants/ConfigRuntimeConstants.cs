40d2cc953bd731f8d7decb7d75e23a8a4a4987ce
anonymous <anonymous>
2020-03-14 15:05:10 -0700

    ap - Added config constants and an NLog.config to the client

---
+++ client/Classes/Constants/ConfigRuntimeConstants.cs
@@ -0,0 +1,28 @@
+using System.Runtime.InteropServices;
+namespace client.Classes.Constants
+{
+    public class ConfigRuntimeConstants
+    {
+        public static readonly string FFMPEG = "ffmpeg";
+        public static readonly string NETWORK = "network";
+        public static readonly string SETTINGS_FILE = "appsettings.json";
+        public static string OS
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return "windows";
+                }
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                {
+                    return "osx";
+                }
+                else
+                {
+                    return "linux";
+                }
+            }
+        }
+    }
+}
